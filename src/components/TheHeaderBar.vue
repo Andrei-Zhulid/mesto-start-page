1ccecb9ef818ed02a8222c03a9f9692580d093fe
Andrei Zhulid <>
2021-07-11 18:38:47 +0300

    Added logo

--- src/components/TheHeaderBar.vue
+++ src/components/TheHeaderBar.vue
@@ -6,11 +6,30 @@
     dark
     class="header"
   >
+    <v-container class="container">
+      <v-row align="center">
+        <v-col class="flex-grow-0">
+          <v-img
+            :src="require('../assets/logo.svg')"
+            alt="Mesto Logo"
+            class="logo"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app-bar>
 </template>
 
 <style scoped>
 .header {
   background: var(--background);
+}
+
+.container {
+  max-width: var(--layout-max-width);
+}
+
+.logo {
+  height: 24px;
 }
 </style>
