35e35280967fd1c34a705f9bd149ccb6a69326e2
Andrei Zhulid <>
2021-07-13 11:43:42 +0300

    Added user name and user avatar to the header

--- src/components/TheHeaderBar.vue
+++ src/components/TheHeaderBar.vue
@@ -15,10 +15,40 @@
             class="logo"
           />
         </v-col>
+
+        <v-spacer></v-spacer>
+
+        <v-col class="flex-grow-0">
+          <v-btn
+            text
+            x-large
+            v-ripple="false"
+            class="px-0"
+          >
+            <span>{{ user.name }}</span>
+            <v-img
+              :src="user.avatar"
+              :alt="user.name"
+              class="avatar ml-4"
+            />
+          </v-btn>
+        </v-col>
       </v-row>
     </v-container>
   </v-app-bar>
 </template>
+
+<script>
+import data from '@/data.json'
+
+export default {
+  data () {
+    return {
+      user: data.user,
+    }
+  },
+}
+</script>
 
 <style scoped>
 .header {
@@ -31,5 +61,10 @@
 
 .logo {
   height: 24px;
+}
+
+.avatar {
+  height: 40px;
+  width: 40px;
 }
 </style>
