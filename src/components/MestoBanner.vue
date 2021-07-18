851fa5c966b0d5fcd2bcaed31526856be6a807b4
Andrei Zhulid <>
2021-07-18 16:55:37 +0300

    Added Mesto banner

---
+++ src/components/MestoBanner.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-sheet
+    dark
+    class="banner d-flex align-center pa-5"
+  >
+    <v-img
+      :src="require('@/assets/logo-long.svg')"
+      position="left"
+      contain
+      alt="Mesto logo"
+      class="logo mr-5"
+    />
+    <slot></slot>
+  </v-sheet>
+</template>
+
+<style scoped>
+.banner {
+  background: var(--background);
+}
+
+.logo {
+  height: 25px;
+  width: 96px;
+}
+</style>
--- src/components/TheMainContent.vue
+++ src/components/TheMainContent.vue
@@ -2,7 +2,29 @@
   <v-container class="pa-0">
     <v-row>
       <v-col>
+        <MestoBanner>
+          <!-- Space between two texts -->
+          <div class="font-weight-bold text-right">{{
+              banner.whiteText
+            }} <span class="secondary-color">{{ banner.limeText }}</span>
+          </div>
+        </MestoBanner>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script>
+import MestoBanner from '@/components/MestoBanner.vue'
+import data from '@/data.json'
+
+export default {
+  components: { MestoBanner },
+
+  data () {
+    return {
+      banner: data.mestoBanner,
+    }
+  },
+}
+</script>
