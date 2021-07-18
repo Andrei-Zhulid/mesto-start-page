b0420c9b3273aaedb023afa51c42da15ac291462
Andrei Zhulid <>
2021-07-18 16:34:43 +0300

    Added Layout component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <v-app>
-    <TheHeaderBar />
-    <v-main>
+    <TheHeaderBar/>
+    <v-main class="background-gray">
+      <TheLayout/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import TheHeaderBar from '@/components/TheHeaderBar'
+import TheLayout from '@/components/TheLayout.vue'
 
 export default {
   name: 'App',
 
   components: {
     TheHeaderBar,
+    TheLayout,
   },
 
   data: () => ({
---
+++ src/components/TheLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="pa-6">
+    <v-container class="layout-width pa-0">
+      <v-row>
+        <v-col md="3">
+        </v-col>
+        <v-col
+          md="9"
+          cols="12"
+        >
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
