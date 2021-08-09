1dd9870f11eab39bb5a51a19cc2fbde02f44e54e
Andrei Zhulid <>
2021-08-09 18:34:38 +0300

    Added MCard component

--- src/components/BlockEventsCard.vue
+++ src/components/BlockEventsCard.vue
@@ -1,39 +1,35 @@
 <template>
-  <v-card
-    :to="event.route"
-    v-ripple="false"
-    class="no-pseudo-opacity pa-4 pb-10"
-  >
-    <div class="d-flex align-center mb-2">
-      <div class="text-body-2 font-weight-bold text-no-wrap">
-        <span class="deep-purple--text text-capitalize">{{ event.date | dateTimeFormat('ddd MMM D') }}</span>
-        &nbsp;<span class="grey-purple--text">/</span>&nbsp;
-        <span class="deep-purple--text">{{ event.date | dateTimeFormat('H:mm') }}</span>
+  <MCard :to="event.route">
+    <template #header>
+      <div class="d-flex align-center">
+        <div class="text-body-2 font-weight-bold text-no-wrap">
+          <span class="deep-purple--text text-capitalize">{{ event.date | dateTimeFormat('ddd MMM D') }}</span>
+          &nbsp;<span class="grey-purple--text">/</span>&nbsp;
+          <span class="deep-purple--text">{{ event.date | dateTimeFormat('H:mm') }}</span>
+        </div>
+        <v-spacer/>
+        <v-chip
+          v-if="event.tag"
+          color="lime"
+          label
+          x-small
+          class="tag_font px-1"
+        >{{ event.tag }}
+        </v-chip>
       </div>
-      <v-spacer/>
-      <v-chip
-        v-if="event.tag"
-        color="lime"
-        label
-        x-small
-        class="tag_font px-1"
-      >{{ event.tag }}
-      </v-chip>
-    </div>
+    </template>
 
-    <span class="two-lines text-subtitle-1 mb-2">{{ event.name }}</span>
+    <template #default>
+      <span class="two-lines text-subtitle-1 mb-2">{{ event.name }}</span>
 
-    <MAvatar
-      :src="event.host.avatar"
-      class="text-body-2 font-weight-bold mb-4"
-    >{{ event.host.name }}
-    </MAvatar>
+      <MAvatar
+        :src="event.host.avatar"
+        class="text-body-2 font-weight-bold"
+      >{{ event.host.name }}
+      </MAvatar>
+    </template>
 
-    <v-footer
-      color="white"
-      absolute
-      class="pa-4 pt-0"
-    >
+    <template #footer>
       <MAvatar
         v-for="(avatar, i) in event.attendees.slice(0, 2)"
         :key="i"
@@ -44,26 +40,28 @@
         v-if="event.attendees.length >= 3"
         :src="event.attendees[2]"
       >
-        <span
-          v-if="event.attendees.length > 3"
-          class="ml-n1"
-        >+{{ event.attendees.length - 3 }}</span>
+            <span
+              v-if="event.attendees.length > 3"
+              class="ml-n1"
+            >+{{ event.attendees.length - 3 }}</span>
       </MAvatar>
 
       <v-spacer/>
 
       <MButtonJoin x-small/>
-    </v-footer>
-  </v-card>
+    </template>
+  </MCard>
 </template>
 
 <script>
 import dateTimeFilter from '@/mixins/dateTimeFilter'
 import MAvatar from '@/components/MAvatar'
 import MButtonJoin from '@/components/MButtonJoin'
+import MCard from '@/components/MCard'
 
 export default {
   components: {
+    MCard,
     MButtonJoin,
     MAvatar,
   },
---
+++ src/components/MCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card
+    :to="to"
+    v-ripple="false"
+    class="no-pseudo-opacity pa-4"
+    :class="$slots.footer ? 'pb-14' : ''"
+  >
+    <slot name="header"/>
+
+    <div :class="$slots.header ? 'mt-2' : ''">
+      <slot/>
+    </div>
+
+    <v-footer
+      v-if="$slots.footer"
+      color="white"
+      absolute
+      class="pa-4 pt-0"
+    >
+      <slot name="footer"/>
+    </v-footer>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    to: Object,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
