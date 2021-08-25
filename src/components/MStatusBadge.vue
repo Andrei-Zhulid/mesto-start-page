13f3f0ff2e1b7877a5ee49482c12f4eb008d7ebf
Andrei Zhulid <>
2021-08-25 15:42:04 +0300

    Bugfix: Wrong status badge position in case user name takes 2 lines

--- src/components/MStatusBadge.vue
+++ src/components/MStatusBadge.vue
@@ -1,15 +1,17 @@
 <template>
-  <v-badge
-    :color="online ? 'lime' : 'grey-purple'"
-    bottom
-    dot
-    tile
-    bordered
-    offset-x="7"
-    offset-y="7"
-  >
-    <slot/>
-  </v-badge>
+  <div>
+    <v-badge
+      :color="online ? 'lime' : 'grey-purple'"
+      bottom
+      dot
+      tile
+      bordered
+      offset-x="7"
+      offset-y="7"
+    >
+      <slot/>
+    </v-badge>
+  </div>
 </template>
 
 <script>
